53d654cea2453a092612d8f65d168a7458b38f57
Valeria <valeria1sokol@gmail.com>
2020-01-25 13:33:54 +0200

    fff

---
+++ js/impact.behind.js
@@ -0,0 +1,95 @@
+    $('#dot0d').click(function () {
+        $('.intro1').animate({
+            opacity: 1
+        }, 500);
+        $('.intro2').animate({
+            opacity: 0
+        }, 500);
+        $('.intro3').animate({
+            opacity: 0
+        }, 500);
+        $('.intro4').animate({
+            opacity: 0
+        }, 500);
+         $('#dot1d').css({
+            transform: 'scale(1)'
+        });
+         $('#dot2d').css({
+            transform: 'scale(1)'
+        });
+         $('#dot3d').css({
+            transform: 'scale(1)'
+        });
+
+    })
+    $('#dot1d').click(function () {
+        $('.intro1').animate({
+            opacity: 0
+        }, 500);
+        $('.intro2').animate({
+            opacity: 1
+        }, 500);
+        $('.intro3').animate({
+            opacity: 0
+        }, 500)
+        $('.intro4').animate({
+            opacity: 0
+        }, 500);
+       
+        $('#dot1d').css({
+            transform: 'scale(2)'
+        });
+         $('#dot2d').css({
+            transform: 'scale(1)'
+        });
+         $('#dot3d').css({
+            transform: 'scale(1)'
+        });
+    })
+    $('#dot2d').click(function () {
+        $('.intro1').animate({
+            opacity: 0
+        }, 500);
+        $('.intro2').animate({
+            opacity: 0
+        }, 500);
+        $('.intro3').animate({
+            opacity: 1
+        }, 500)
+        $('.intro4').animate({
+            opacity: 0
+        }, 500);
+         $('#dot1d').css({
+            transform: 'scale(1)'
+        });
+         $('#dot2d').css({
+            transform: 'scale(2)'
+        });
+         $('#dot3d').css({
+            transform: 'scale(1)'
+        });
+    })
+    $('#dot3d').click(function () {
+        $('.intro1').animate({
+            opacity: 0
+        }, 500);
+        $('.intro2').animate({
+            opacity: 0
+        }, 500);
+        $('.intro3').animate({
+            opacity: 0
+        }, 500)
+        $('.intro4').animate({
+            opacity: 1
+        }, 500);
+          $('#dot1d').css({
+            transform: 'scale(1)'
+        });
+         $('#dot2d').css({
+            transform: 'scale(1)'
+        });
+         $('#dot3d').css({
+            transform: 'scale(2)'
+        });
+    })
+ 
--- js/script.js
+++ js/script.js
@@ -9,31 +9,16 @@ if (widthW >= '1024') {
         var k = 1;
         openB.find(".marie .touch").click(function () {
             if (k == 1) {
-                if (widthW <= '1024') {
-                    $('.about_box').animate({
-                        padding: '3.1vw 5.4vw 18.1vw 6.5vw',
-                    }, 700);
-                } else {
-                    $('.about_box').animate({
-                        top: '0vw',
-                        padding: '3.1vw 5.4vw 7.1vw 6.5vw',
-                    }, 700);
-                }
-                if (widthH <= '800') {
-                    $('.about_box').animate({
-                        padding: '3.1vw 5.4vw 5.1vw 6.5vw',
-                        top:'-4vw'
-                    }, 500);
-                } else {
-                    $('.about_box').animate({
-                        top: '0vw',
-                        padding: '3.1vw 5.4vw 7.1vw 6.5vw',
-                    }, 500);
-                }
-                $('.about_box').css({
-                    zIndex: '3',
-                    borderRadius: '6.5vw 0 0 0'
-                });
+                $('.about_box_m').animate({
+                    opacity: 1,
+                }, 1000);
+                $('.about_box_m').css({
+                    zIndex: '3'
+                })
+                $('.about_box').animate({
+                    opacity: 0
+                }, 800)
+
                 $('.pierre').css({
                     zIndex: '1'
                 });
@@ -43,28 +28,17 @@ if (widthW >= '1024') {
                 $('.shadowGreen').animate({
                     opacity: '0.8'
                 });
-                $(".slide-toggled-element").slideToggle("slow");
-                $('.about_p').css({
-                    display: 'none'
-                })
                 k = 0;
             } else if (k == 0) {
-
-                if (widthW <= '1024') {
-                    $('.about_box').animate({
-                        padding: '3.1vw 1.4vw 3.1vw 15.5vw',
-                        zIndex: '1'
-                    }, 500);
-                } else {
-                    $('.about_box').animate({
-                        top: '0vw',
-                        padding: '3.1vw 2.4vw 3.1vw 11.5vw',
-                        zIndex: '1'
-                    }, 500);
-                }
-                $('.about_box').css({
-                    borderRadius: '6.5vw 0 0 6.5vw'
-                });
+                $('.about_box_m').animate({
+                    opacity: 0
+                }, 800)
+                $('.about_box_m').animate({
+                    zIndex: 0
+                }, 1000)
+                $('.about_box').animate({
+                    opacity: 1
+                }, 800)
                 $('.shadow').animate({
                     opacity: '0'
                 });
@@ -74,42 +48,21 @@ if (widthW >= '1024') {
                 $('.pierre').css({
                     zIndex: '3'
                 });
-                $(".slide-toggled-element").slideToggle("slow");
-                $('.about_p').css({
-                    display: 'block'
-                })
                 k = 1;
             }
         });
 
         openB.find(".pierre .touchP").click(function () {
             if (k == 1) {
-
-                if (widthW <= '1024') {
-                    $('.about_box').animate({
-                        padding: '3.1vw 5.4vw 23.1vw 6.5vw',
-                    }, 1000);
-                } else {
-                    $('.about_box').animate({
-                        top: '0vw',
-                        padding: '3.1vw 5.4vw 10.1vw 6.5vw',
-                    }, 1000);
-                }
-                 if (widthH <= '800') {
-                    $('.about_box').animate({
-                        padding: '3.1vw 5.4vw 8vw 6.5vw',
-                        top:'-4vw'
-                    }, 500);
-                } else {
-                    $('.about_box').animate({
-                        top: '0vw',
-                        padding: '3.1vw 5.4vw 7.1vw 6.5vw',
-                    }, 500);
-                }
-                $('.about_box').css({
-                    zIndex: '3',
-                    borderRadius: '6.5vw 0 0 0',
-                });
+                $('.about_box_p').animate({
+                    opacity: 1,
+                }, 1000);
+                $('.about_box_p').css({
+                    zIndex: '3'
+                })
+                $('.about_box').animate({
+                    opacity: 0
+                }, 800)
                 $('.marie').css({
                     zIndex: '2'
                 });
@@ -120,35 +73,19 @@ if (widthW >= '1024') {
                     opacity: '0.8'
                 });
 
-                $(".slide-toggled-element2").slideToggle("slow");
-                $('.about_p').css({
-                    display: 'none'
-                })
-                $('.about_Maria').css({
-                    display: 'none'
-                });
-                $('.about_Pierre').css({
-                    display: 'flex'
-                })
+
                 k = 0;
             } else if (k == 0) {
+                $('.about_box_p').animate({
+                    opacity: 0
+                }, 800)
+                $('.about_box_p').animate({
+                    zIndex: 0
+                }, 1000)
+                $('.about_box').animate({
+                    opacity: 1
+                }, 800)
 
-                if (widthW <= '1024') {
-                    $('.about_box').animate({
-                        padding: '3.1vw 1.4vw 3.1vw 15.5vw',
-                        zIndex: '1'
-                    }, 500);
-                } else {
-                    $('.about_box').animate({
-                        top: '0vw',
-                        padding: '3.1vw 2.4vw 3.1vw 11.5vw',
-                        zIndex: '1'
-                    }, 500);
-                }
-                $('.about_box').css({
-                    borderRadius: '6.5vw 0 0 6.5vw'
-
-                }, );
                 $('.shadow').animate({
                     opacity: '0'
                 });
@@ -158,16 +95,7 @@ if (widthW >= '1024') {
                 $('.marie').css({
                     zIndex: '3'
                 });
-                $('.about_Maria').css({
-                    display: 'flex'
-                });
-                $('.about_Pierre').css({
-                    display: 'none'
-                })
-                $(".slide-toggled-element2").slideToggle("slow");
-                $('.about_p').css({
-                    display: 'block'
-                })
+
                 k = 1;
             }
         });
@@ -181,9 +109,7 @@ if (widthW <= '1023') {
         var openB = $(this);
         var k = 1;
         openB.find(".marie .touch").click(function () {
-            $('.about_box_m').css({
-                zIndex: 2
-            })
+
             if (k == 1) {
                 $('.about_box_m').animate({
                     opacity: 1,
